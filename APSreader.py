a19678456938b468898d485b4f54bc60b493b98b
Daniel O'Sullivan <danielosullivan2007@gmail.com>
2017-09-13 17:52:27 +0100

    added new views to the timeseries.py plotter

---
+++ APSreader.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 13 11:49:19 2017
+
+@author: eardo
+"""
+
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MultipleLocator, FormatStrFormatter
+import matplotlib.dates as mdates
+
+
+
+
+
+# =============================================================================
+# from bokeh.plotting import figure, show, output_file, save
+# from bokeh.mpl import to_bokeh
+# bk = to_bokeh(fig)
+# show(bk)
+# =============================================================================
--- timeseries.py
+++ timeseries.py
@@ -7,15 +7,26 @@ Created on Tue Sep 12 13:22:25 2017
 import os 
 import pandas as pd
 from datetime import datetime
-#os.chdir('C:\\Users\\eardo\\Desktop\\Farmscripts\\')
-os.chdir('C:\\Users\\useradmin\\Desktop\\Farmscripts\\')
 from myfuncs import num2words
 import seaborn as sns
+import matplotlib.pyplot as plt
+import socket
+
+host = socket.gethostname()
+
+if host == 'see4-234':
+    os.chdir('C:\\Users\\eardo\\Desktop\\Farmscripts\\')
+    
+    picdir = 'C:\\Users\\eardo\\Desktop\\Farmscripts\\Pickels\\'
+
+else: 
+    os.chdir('C:\\Users\\useradmin\\Desktop\\Farmscripts\\')
+    picdir = 'C:\\Users\\useradmin\\Desktop\\Farmscripts\\Pickels\\'
+
 
 
 df=pd.read_csv('observations.csv')
-#picdir = 'C:\\Users\\eardo\\Desktop\\Farmscripts\\Pickels\\'
-picdir = 'C:\\Users\\useradmin\\Desktop\\Farmscripts\\Pickels\\'
+
 
 fmt='%y%m%d'
 new_cols = ['date','Datestr',
@@ -52,7 +63,7 @@ processed.set_index('date', inplace =True)
 
 data ={i: pd.read_csv(picdir + "data at "+num2words[i]+".csv")for i in range(-25, -10, 5)}
     
-minus15=data[-20]   
+minus15=data[-15]   
 minus15['date']=pd.to_datetime(minus15['2'], box =False).dt.date
 minus15.set_index('date', inplace =True)
 
@@ -102,17 +113,66 @@ myFmt = mdates.DateFormatter('%m/%d')
 from matplotlib.ticker import MultipleLocator, FormatStrFormatter
 
 majorLocator = MultipleLocator(4)
-#majorFormatter = FormatStrFormatter('%d')
+
 minorLocator = MultipleLocator(5)
 
 ax.xaxis.set_major_locator(majorLocator)
-ax.xaxis.set_major_formatter(majorFormatter)
 
-# for the minor ticks, use no labels; default NullFormatter
 ax.xaxis.set_minor_locator(minorLocator)
 ax.set_ylabel('log10 [INP]')
 ax.xaxis.set_major_formatter(myFmt)
 ax.legend(['No activity','Agricultural activity'], loc = 'top right')
+#%%
+
+
+APS = pd.read_pickle(picdir+'aps.p')
+df_APS =APS
+myFmt = mdates.DateFormatter('%m/%dm')
+
+APS_cols=[u'Date',
+       u'Start Time', u'datetime', u'<0.523', u'0.542', u'0.583', u'0.626', u'0.673', u'0.723', u'0.777', u'0.835',
+       u'0.898', u'0.965', u'1.037', u'1.114', u'1.197', u'1.286', u'1.382',
+       u'1.486', u'1.596', u'1.715', u'1.843', u'1.981', u'10.37', u'11.14',
+       u'11.97', u'12.86', u'13.82', u'14.86', u'15.96', u'17.15', u'18.43',
+       u'19.81', u'2.129', u'2.288', u'2.458', u'2.642', u'2.839', u'3.051',
+       u'3.278', u'3.523', u'3.786', u'4.068', u'4.371', u'4.698', u'5.048',
+       u'5.425', u'5.829', u'6.264', u'6.732', u'7.234', u'7.774', u'8.354',
+       u'8.977', u'9.647']
+
+df_APS=df_APS[APS_cols]
+df_APS['Total'] = df_APS.loc[:,u'0.542':u'9.647'].sum(axis=1)
+df_APS['day']=df_APS.index.day
+df_APS.set_index('datetime', drop =True, inplace=True)
+
+APS = df_APS['Hour'] 
+SMPS = pd.read_pickle(picdir + 'smps.p')
+df_SMPS =SMPS
+df_SMPS['Total']=df_SMPS.loc[:,' 14.1':'736.5'].sum(axis=1)
+df_SMPS.set_index('datetimes', drop =True, inplace =True)
+
+
+fig, ax=plt.subplots()
+final.set_index(final['3'], inplace =True)
+final.index = pd.to_datetime(final.index)
+ax=final.INP.plot(marker ='o', color ='b', alpha = 0.8)
+ax.set_ylim(-2,2)
+
+myFmt = mdates.DateFormatter('%m-%d')
+ax.xaxis.set_major_formatter(myFmt)
+majorLocator = MultipleLocator(4)
+minorLocator = MultipleLocator(2)
+ax.xaxis.set_major_locator(majorLocator)
+ax.tick_params(axis='x',which='minor',bottom='on')
+
+
+ax2=ax.twinx()
+ax2=plt.plot(df_APS.Total.resample('0.5h').sum(), color='red')
+plt.ylim(-100,10000)
+plt.minorticks_on()
+ax.xaxis.set_minor_locator(minorLocator)
+
+#%%
+#APS_topiv = df['APS']
 
 
 
