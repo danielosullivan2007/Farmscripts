101fdeb1078ee81653b702c00347550d8bd876d6
Daniel O'Sullivan <danielosullivan2007@gmail.com>
2017-10-24 13:03:52 +0100

    major cleanup of files into folders

---
+++ directories.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 24 11:26:23 2017
+
+@author: eardo
+"""
+
+import socket
+host=socket.gethostname()
+
+if host == 'see4-234':
+    farm_pickles = 'C:\\Users\\eardo\\Desktop\\farmscripts\\Pickels\\'
+    farm_figs= 'C:\\Users\\eardo\\Desktop\\farmscripts\\Figures\\' 
+    
+    #Barbados Data#
+    source = 'V:\\Data Dump\\'
+    Organized ='V:\\Barbados_Data\\'
+    bar_pickles =  'V:\\Pickels\\'
+    bar_figs  = 'V:\\Figures\\'
+    
+
+    
